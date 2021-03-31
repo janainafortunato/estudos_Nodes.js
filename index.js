b3b8accbf563f41172fe2e9ea17a613fea272cd9
janainafortunato <janaina.projetos@live.com>
2021-03-31 10:15:47 -0300

    ajuste de porta para roda o projeto

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+const { request, response } = require('express');
 const express = require('express'); //require
-
 const app = express();
 
-console.log(app);
+//console.log(app);
+
+app.get('/', (request, response) => {
+    response.send('Olá Mundo Janaina Fortunato!!!!!!');
+})
+
+app.listen(8080, () => {
+    console.log('Servidor rodando!');
+})
