72a041c277329ffe3457b83ef4dce1410270c7ee
janainafortunato <janaina.projetos@live.com>
2021-03-31 12:04:21 -0300

    criando metodos no insomnia

--- index.js
+++ index.js
@@ -4,10 +4,49 @@ const app = express();
 
 //console.log(app);
 
-app.get('/', (request, response) => {
-    response.send('Olá Mundo Janaina Fortunato!!!!!!');
-})
+app.get('/projects', (request, response) => {
 
-app.listen(8080, () => {
-    console.log('Servidor rodando!');
-})
+    const {title, owner} = request.query;
+    console.log(title)
+    console.log(owner)
+
+   return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 100'
+    ])
+});
+
+app.post('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3',
+        'Projeto 4',
+        'Projeto 5'
+    ])
+});
+
+app.put('/projects/:id', (request, response) => {
+    
+    const params = request.params;
+    console.log(params)
+
+    return response.json([
+        'Projeto 50',
+        'Projeto 51',
+        'Projeto 52',
+        'Projeto 53',
+        'Projeto 54'
+    ]);
+});
+
+app.delete('/projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 50',
+        'Projeto 2',
+    ]);
+});
+
+
+app.listen(8080);
